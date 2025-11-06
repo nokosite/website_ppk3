474f34da2c05f79d064b5bfdb03ed7ed6eee70eb
MK <mahesyt@gmail.com>
2025-11-07 04:23:39 +0800

    test

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -20,9 +20,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('build/assets/app-e7cecb2c.css') }}" >
-    <script src="{{ asset('build/assets/app-cdac73cb.js') }}"></script>
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 
